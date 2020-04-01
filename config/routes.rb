235c0b0287583b899429867095aff78726a58489
福岡和樹 <kazuki@mba.local>
2020-04-01 11:51:26 +0900

    トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'ramen_stores#top'
+  root to: 'top#top'
 
-  get 'ramens/index', to: 'ramen_stores#index'
+  get 'ramens', to: 'ramen_stores#index'
   get 'ramens/:id', to: 'ramen_stores#show'
-
-  get 'ramens/comments/:id', to: 'ramen_store_comments#show'
-
-  get 'ramens/search', to: 'ramen_store_searches#index'
 
   get 'users/new', to: 'users#new'
   get 'users/id', to: 'users#show'
