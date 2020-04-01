a8731b6905b6b55f2de66230fd97f71c25def150
福岡和樹 <kazuki@mba.local>
2020-04-01 10:24:34 +0900

    トップページ

---
+++ app/controllers/ramen_stores_controller.rb
@@ -0,0 +1,16 @@
+class RamenStoresController < ApplicationController
+
+  def top
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+    # @ramen = Ramen.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'ramens#top'
-  get 'ramens/index', to: 'ramens#index'
-  get 'ramens/:id', to: 'ramens#show'
-  
+  root to: 'ramen_stores#top'
+
+  get 'ramens/index', to: 'ramen_stores#index'
+  get 'ramens/:id', to: 'ramen_stores#show'
+
+  get 'ramens/comments/:id', to: 'ramen_store_comments#show'
+
+  get 'ramens/search', to: 'ramen_store_searches#index'
+
   get 'users/new', to: 'users#new'
   get 'users/id', to: 'users#show'
 end
