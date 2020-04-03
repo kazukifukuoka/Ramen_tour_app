c88deabcc86ba34ad79ae4931bb99ce68e61a910
福岡和樹 <kazuki@mba.local>
2020-04-03 22:31:39 +0900

    ページ一覧

--- app/models/ramen_store.rb
+++ app/models/ramen_store.rb
@@ -8,11 +8,16 @@ class RamenStore < ApplicationRecord
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
 
   validates :name, presence: true
-  validates :sale, presence: true
-  validates :address, presence: true
-  validates :parking_space, presence: true
-  validates :phone_number, presence: true, uniqueness: true, format: { with: VALID_PHONE_REGEX }
-  validates :sns, presence: true
+  validates :postcode, presence: true
   validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, uniqueness: true, format: { with: VALID_PHONE_REGEX }
+  validates :sale, presence: true
+  validates :holiday, presence: true
+  validates :seat, presence: true
+  validates :access, presence: true
+  validates :parking_space, presence: true
+  validates :sns, presence: true
   validates :content, presence: true
 end
--- db/migrate/20200401053900_create_ramen_stores.rb
+++ db/migrate/20200401053900_create_ramen_stores.rb
@@ -2,13 +2,19 @@ class CreateRamenStores < ActiveRecord::Migration[6.0]
   def change
     create_table :ramen_stores do |t|
       t.string :name, null: false, index: true
-      t.text :content, null: false
-      t.string :sale, null: false
-      t.string :address, null: false
-      t.string :sns, null: false
-      t.string :phone_number, null: false
-      t.string :parking_space, null: false
+      t.string :postcode, null: false
       t.integer :prefecture_id, null: false, index: true
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :phone_number, null: false
+      t.string :sale, null: false
+      t.string :holiday, null: false
+      t.string :seat, null: false
+      t.string :access, null: false
+      t.string :parking_space, null: false
+      t.string :sns, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
