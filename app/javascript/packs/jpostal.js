308929c54a18183b05ce663d6f8e340388aa0bb3
福岡和樹 <kazuki@mba.local>
2020-04-04 14:10:32 +0900

    js

---
+++ app/javascript/packs/jpostal.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#ramen_store_postcode').jpostal({
+    postcode : [
+      '#ramen_store_postcode'
+    ],
+    address : {
+      '#ramen_store_prefecture_id'  : '%3',
+      '#ramen_store_city'  : '%4%5'
+    }
+  });
+});
