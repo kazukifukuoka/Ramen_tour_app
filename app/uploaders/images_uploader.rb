4766117c6739dbf53e54a13eb18cfe38c2d17449
福岡和樹 <kazuki@mba.local>
2020-04-06 19:40:59 +0900

    no_image

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -58,7 +58,6 @@ class ImagesUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "thumb100_default.png"
     # ActionController::Base.helpers.asset_path("images/" + [version_name, "default.png"].compact.join('_'))
     # "/images/" + [version_name, "default.png"].compact.join('_')
   end
