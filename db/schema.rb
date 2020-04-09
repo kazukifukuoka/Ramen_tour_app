76a5fe03f8c2b16138423073ea872af7da0bd4c5
福岡和樹 <kazuki@mba.local>
2020-04-09 21:02:36 +0900

    sign_up_mailer

--- db/schema.rb
+++ db/schema.rb
@@ -93,6 +93,10 @@ ActiveRecord::Schema.define(version: 2020_04_06_033846) do
     t.string "image"
     t.text "profile"
     t.boolean "is_admin"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
