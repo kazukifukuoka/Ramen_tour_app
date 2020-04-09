c92d786929d1f7de67c7aed3f74baf1ad2a012f2
福岡和樹 <kazuki@mba.local>
2020-04-09 21:02:36 +0900

    sign_uo_mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+  }
+
+  devise_scope :user do
+    put 'confirmation', to: 'users/confirmations#show', as: :back_confirmation
+  end
   root to: 'top#top'
 
   resources :ramen_stores
+
 end
