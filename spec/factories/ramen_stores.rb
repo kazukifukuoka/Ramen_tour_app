1e23538aadc206453440bec6101b67905d4615cd
福岡和樹 <kazuki@mba.local>
2020-04-25 22:20:40 +0900

    request_spec

--- spec/factories/ramen_stores.rb
+++ spec/factories/ramen_stores.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :ramen_store do
     sequence(:name) { |n| "店舗#{n}" }
     postcode { '123-4567' }
-    prefucture_id { '1' }
+    prefecture_id { '1' }
     city { 'A市B町' }
     address { 'C番地' }
     building { 'Dビル101号室' }
-    sequence(:phonenumber) { |n| "123-45-678#{n}" }
+    sequence(:phone_number) { |n| "0123-45-678#{n}" }
     sale { '18時から20時まで' }
     holiday { '毎週月曜日' }
     seat { 'カウンター10席' }
@@ -14,6 +14,8 @@ FactoryBot.define do
     parking_space { '近くにパーキングエリア有り' }
     sns { 'Twitter: @テスト' }
     content{ 'テストです' }
+    tag_list {'テスト'}
+    created_at { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     association :user
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,10 +34,12 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+require 'devise'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.extend AuthenticationMacros
 
-  config.include Devise::TestHelpers, type: :controller
-  config.include ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
