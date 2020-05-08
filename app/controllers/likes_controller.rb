a5aa0b7ae302fc0ac95d717016696e7183f8771a
福岡和樹 <kazuki@mba.local>
2020-05-08 15:29:49 +0900

    タブで店舗詳細、メニュー、画像を選択できる

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,7 @@
 class LikesController < ApplicationController
   def create
-
     @ramen_store = RamenStore.find(params[:ramen_store_id])
     @like = current_user.likes.create!(ramen_store_id: @ramen_store.id)
-
   end
 
   def destroy
