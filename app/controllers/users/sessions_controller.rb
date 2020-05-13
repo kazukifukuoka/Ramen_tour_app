b1e0fc4fa406cc5d03c1afa9523c4b49a91b63b9
福岡和樹 <kazuki@mba.local>
2020-05-13 12:37:54 +0900

    マイページのレビュータブを作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::SessionsController < Devise::SessionsController
   before_action :request_path
-  MY_PAGE_PER = 6
+  MY_STORE_PER = 6
+  MY_REVIEW_PER = 5
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -22,7 +23,8 @@ class Users::SessionsController < Devise::SessionsController
 
   def show
     @user = User.find(params[:id])
-    @post_ramen_store = current_user.ramen_stores.page(params[:page]).per(MY_PAGE_PER)
+    @post_ramen_store = current_user.ramen_stores.page(params[:store_page]).per(MY_STORE_PER)
+    @post_reviews = current_user.ramen_store_reviews.page(params[:review_page]).per(MY_REVIEW_PER)
   end
 
   def new_guest
